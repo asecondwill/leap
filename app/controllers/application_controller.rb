c53675affa6818e8856a08f8d74bd2a05e9a3997
Will <will@kindleman.com.au>
2023-11-07 19:07:22 +0000

    fixes

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < ActionController::Base
+  before_action :set_current_user  
+  # around_action :set_time_zone, if: :current_user
+
+  def set_current_user
+    # TODO: Devise used to do this, is something borked?
+    @current_user = current_user if current_user
+  end
+  
+  # def set_time_zone(&block)
+  #   Time.use_zone(current_user.time_zone, &block)
+  # end  
+end
